6f21231a1777407a527ee543f4d448d682c067f4
Moritz <1pcmoritz@gmail.com>
2026-06-27 19:40:45 +0200

    Add files via upload

---
+++ CalculatorV2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+
+namespace CalculatorV2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to Calculator v2");
+                Console.WriteLine("--------------------------");
+                Thread.Sleep(2000);
+                Console.Clear();
+                Console.Write("Enter your exercise: ");
+                string exercise = Console.ReadLine();
+                var table = new DataTable();
+                var result = table.Compute(exercise, "");
+                Console.WriteLine(result);
+                Console.Write("Do you want to end the calculator (yes/no)?: ");
+                string frage = Console.ReadLine();
+                if (frage == "yes" || frage == "Yes")
+                {
+                    Console.WriteLine("Ending... ");
+                    Thread.Sleep(2000);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Back to start...");
+           
+                }
+            }
+        }
+    }
+}
